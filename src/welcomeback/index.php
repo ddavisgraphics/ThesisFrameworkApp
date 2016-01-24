aa9e991e162e468ec7b7c08ab345ade56accbb0a
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-24 14:59:58 -0500

    welcome back login

---
+++ src/welcomeback/index.php
@@ -0,0 +1,37 @@
+<?php
+    require_once "../includes/engine.php";
+    templates::display('header');
+?>
+<section class="wrapper">
+    <div class="container">
+        <h2> Welcome Back </h2>
+        <p> What would you like to do?  </p>
+
+        <h2> Learning Modules </h2>
+        <ul>
+            <li> <a> Test </a></li>
+        </ul>
+
+        <h2> Challenges </h2>
+        <ul>
+            <li> <a> Test </a></li>
+        </ul>
+
+        <h2> Tutorial Series </h2>
+        <ul>
+            <li><a href=""> Engine </a></li>
+            <li><a href=""> Meteor </a></li>
+        </ul>
+
+        <h2> Tutorial Series </h2>
+        <ul>
+            <li><a href=""> Test </a></li>
+        </ul>
+    </div>
+</section>
+
+<?php
+    templates::display('footer');
+?>
+
+
