446215e86fb14842c1d6b98f601441735d837ad1
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-23 23:12:47 -0500

    server and sql files for commit

---
+++ SQLFiles/baseTables.sql
@@ -0,0 +1,55 @@
+-- Users Session Saves
+-- -------------------------------------------------
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session`(
+    `sID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) NOT NULL,
+    `ipAddr` varchar(50) NOT NULL,
+    `occupation` varchar(200) DEFAULT NULL,
+    `html` boolean NOT NULL DEFAULT 0,
+    `job` varchar(200) DEFAULT NULL,
+    `degree` varchar(300) DEFAULT NULL,
+    PRIMARY KEY(`sID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Users Feedback
+-- -------------------------------------------------
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE `feedback`(
+    `fID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+    `ipAddr` varchar(50) NOT NULL,
+    `username` varchar(50) NOT NULL,
+    `rating` tinyint(3) NULL,
+    `feedback` text NULL,
+    PRIMARY KEY(`fID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Setup Survey
+-- -------------------------------------------------
+DROP TABLE IF EXISTS `setupSurvey`;
+CREATE TABLE `setupSurvey`(
+    `ssID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+    `ipAddr` varchar(50) NOT NULL,
+    `username` varchar(50) NOT NULL,
+    `rating` tinyint(3) NULL,
+    `feedback` text NULL,
+    `framework` varchar(50) NULL,
+    PRIMARY KEY(`ssID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Setup Survey
+-- -------------------------------------------------
+DROP TABLE IF EXISTS `databaseSurvey`;
+CREATE TABLE `databaseSurvey`(
+    `dbID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+    `ipAddr` varchar(50) NOT NULL,
+    `username` varchar(50) NOT NULL,
+    `rating` tinyint(3) NULL,
+    `feedback` text NULL,
+    `code` text NULL,
+    `framework` varchar(50) NULL,
+    PRIMARY KEY(`dbID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
---
+++ SQLFiles/setup.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS `learningApp`;
+CREATE DATABASE IF NOT EXISTS `learningApp`;
+GRANT ALL PRIVILEGES ON `learningApp`.* TO 'username'@'localhost';
+USE `learningApp`;
