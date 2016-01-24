bd90fff392376594f652f9760ef8d7bad78cba17
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-23 23:10:41 -0500

    vagrant and template files

---
+++ template/default/templateFooter.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p> Copyright <?php print Date('Y'); ?> - David J. Davis</p>
+    </div>
+</footer>
+
+<?php recurseInsert("includes/footerIncludes.php","php") ?>
+<?php recurseInsert("footerIncludes.php","php") ?> <!-- Page Specific -->
+</body>
+</html>
---
+++ template/default/templateHeader.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{local var="pageName"}</title>
+
+    <!-- Meta Information -->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="HandheldFriendly" content="True">
+
+    <!-- Author, Description, Favicon, and Keywords -->
+    <meta name="author" content="{local var="meta_authors"}">
+    <meta name="description" content="{local var="meta_description"}">
+    <meta name="keywords" content="{local var="meta_keywords"}">
+
+    <!-- Project Specific Head Includes -->
+    <?php recurseInsert("headerIncludes.php","php") ?> <!-- Page Specific -->
+    <?php recurseInsert("includes/headerIncludes.php","php") ?> <!-- Project Wide -->
+</head>
+
+<body>
+
+<header>
+    <div class="container">
+        <div class="siteTitle">
+            <h1> Learning<span>App</span> </h1>
+        </div>
+        <div class="nav-toggle mobile-only">
+            <a href="javascript:void(0);"> <i class="fa fa-bars"></i> </a>
+        </div>
+    </div>
+</header>
+
+<nav class="wrapper nav hidden">
+    <div class="container">
+        <?php recurseInsert("includes/nav.php","php") ?>
+    </div>
+</nav>
