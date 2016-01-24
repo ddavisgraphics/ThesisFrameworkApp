de83941da0afc65254847f04859e598ef70026a1
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-24 15:21:11 -0500

    adding page vars for ajax

---
+++ src/setup/pageVars.php
@@ -0,0 +1,13 @@
+<?php
+    $localvars = localvars::getInstance();
+
+    $localvars->set('sessionUsername',session::get('username'));
+    $localvars->set('ipAddr', $_SERVER['REMOTE_ADDR']);
+    $localvars->set('pageName', 'Setup -');
+?>
+<!--set JS Variables -->
+<script>
+    var sessionUser = '{local var="sessionUsername"}';
+    var ipAddr = '{local var="ipAddr"}';
+    var pageName = '{local var="pageName"}';
+</script>
